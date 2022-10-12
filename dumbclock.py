0f2ab5fc812f0d1d744e17b20ec6b5564dcfde2c
trungquoc08 <97213432+trungquoc08@users.noreply.github.com>
2022-10-12 10:57:47 +0700

    Add files via upload

---
+++ dumbclock.py
@@ -0,0 +1,20 @@
+
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+def time():
+    string = strftime("%I:%M:%S %p")
+    label.config(text = string)
+    label.after(1000, time)
+
+label = Label(root, font = ("ds-digital" , 100) , background = "black", foreground = "blue")
+label.pack(anchor = "center")
+
+time()
+
+mainloop()
